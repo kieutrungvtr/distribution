dff83611d951984a28391d4799576a3f87aaedde
Admin <admin@Kieu-Trungs-Macbook-Pro.local>
2024-08-08 10:40:06 +0700

    custom table_prefix_deleted_at

--- src/app/Models/BaseModel.php
+++ src/app/Models/BaseModel.php
@@ -4,8 +4,32 @@
 namespace PLSys\DistrbutionQueue\App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Builder;
 
 class BaseModel extends Model
 {
+    protected static function boot()
+    {
+        parent::boot();
 
+        static::addGlobalScope(new OnlyActiveModels());
+    }
+
+}
+
+class OnlyActiveModels implements Scope
+{
+    public function apply(Builder $builder, Model $model)
+    {
+        //Get column name deleted_at was qualify with table name
+        $table = $model->getTable();
+        if (substr($table, -1) === 's') {
+            $prefixColumn = substr($table, 0, -1);
+        } else {
+            $prefixColumn = $table;
+        }
+        $deletedAtColumn = $prefixColumn . '_deleted_at';
+        $builder->whereNull($deletedAtColumn);
+    }
 }
